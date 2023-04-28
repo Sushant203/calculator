a334198640aa5a161a3b3ff803a3192b2023df49
Sushant <128221017+Sushant212@users.noreply.github.com>
2023-04-28 18:31:25 +0545

    keyboard functionality added

--- script1.js
+++ script1.js
@@ -4,12 +4,88 @@ const buttons = document.querySelectorAll("button");
 let string = "";
 
 document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      input.value = eval(string);
+      string = input.value;
+    }
+    if (e.key === "Backspace") {
+      string = string.slice(0, -1);
+      input.value = string;
+    }
+    if (e.key === "Escape") {
+      string = "";
+      input.value = "";
+    }
+    if (e.key === "0") {
+      string += "0";
+      input.value = string;
+    }
+    if (e.key === "1") {
+      string += "1";
+      input.value = string;
+    }
+    if (e.key === "2") {
+      string += "2";
+      input.value = string;
+    }
+    if (e.key === "3") {
+      string += "3";
+      input.value = string;
+    }
+    if (e.key === "4") {
+      string += "4";
+      input.value = string;
+    }
+    if (e.key === "5") {
+      string += "5";
+      input.value = string;
+    }
+    if (e.key === "6") {
+      string += "6";
+      input.value = string;
+    }
+    if (e.key === "7") {
+      string += "7";
+      input.value = string;
+    }
+    if (e.key === "8") {
+      string += "8";
+      input.value = string;
+    }
+    if (e.key === "9") {
+      string += "9";
+      input.value = string;
+    }
+    if (e.key === "+") {
+      string += "+";
+      input.value = string;
+    }
+    if (e.key === "-") {
+      string += "-";
+      input.value = string;
+    }
+    if (e.key === "*") {
+      string += "*";
+      input.value = string;
+    }
+    if (e.key === "/") {
+      string += "/";
+      input.value = string;
+    }
+    if (e.key === ".") {
+      string += ".";
+      input.value = string;
+    }
+  });
   document.querySelector(".calculator").addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
       const buttonValue = e.target.innerHTML;
       switch (buttonValue) {
         case "=":
           input.value = eval(string);
+          string = input.value;
           break;
         case "Ac":
           string = "";
